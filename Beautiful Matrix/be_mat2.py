ae449dc6999649160a3037c71473df58fbe77a2c
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-02-28 16:43:23 +0300

    Updated version of the problem has been added

---
+++ Beautiful Matrix/be_mat2.py
@@ -0,0 +1,17 @@
+x=[]
+try:
+    while True:
+        x.append(list(map(int,input().split())))
+except EOFError:
+    pass
+ 
+for i in range(len(x)):
+    if 1 in x[i]:
+        j=x[i].index(1)
+        y=[i,j]
+        break
+i=y[0]
+j=y[1]
+count=abs(i-2)+abs(j-2)
+ 
+print(count)
