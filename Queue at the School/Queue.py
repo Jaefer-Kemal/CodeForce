77afd0bfe585e498f2546a0dd015c17636b88581
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-02-28 12:39:52 +0300

    changed

---
+++ Queue at the School/Queue.py
@@ -0,0 +1,22 @@
+n, t=map(int,input().split())
+q=list(input().strip())
+
+while t>0:
+    l=0
+    r=l+1
+    t
+    while r < n:
+        if q[l]=="B" and q[r]=="G":
+            q[l]="G"
+            q[r]="B"
+            l=r+1
+            r=l+1
+        else:
+            l+=1
+            r=l+1
+    t-=1
+    
+print("".join(q))
+    
+        
+    
